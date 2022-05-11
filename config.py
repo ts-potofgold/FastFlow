8a04061d99d2ff93bbd09009bf402b2ba2e0a260
ts-thiensean <88038848+ts-potofgold@users.noreply.github.com>
2022-05-11 17:05:23 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
-mvtec_path = "/content/mvtec_anomaly"
+mvtec_path = "/content/drive/MyDrive/boschanomaly"
 # "cait_m48_448" or "deit_base_distilled_patch16_384" is supported.
 backbone = "cait_m48_448"
 device = "cuda:0"
-batch_size = 32
-nb_epoch = 7 # original 500
+batch_size = 16 # original 32
+nb_epoch = 10 # original 500
 learning_rate = 0.001 ## original 2e-4
-warmup_epoch = 2
-validate_per_epoch = 50
+warmup_epoch = 4
+validate_per_epoch = 5 # original 50
 
 clamp = 0.15
 clamp_activation = "ATAN"
