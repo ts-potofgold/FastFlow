e2695972b2ee22323e0d4995322fa2a4da3e2142
ts-thiensean <88038848+ts-potofgold@users.noreply.github.com>
2022-05-11 15:22:38 +0800

    Update config.py

--- config.py
+++ config.py
@@ -1,11 +1,11 @@
 mvtec_path = "/content/mvtec_anomaly"
 # "cait_m48_448" or "deit_base_distilled_patch16_384" is supported.
 backbone = "cait_m48_448"
-device = "cuda:0"
+device = "cuda:1"
 batch_size = 32
-nb_epoch = 38
-learning_rate = 2e-4
-warmup_epoch = 4
+nb_epoch = 7 # original 500
+learning_rate = 0.001 ## original 2e-4
+warmup_epoch = 2
 validate_per_epoch = 50
 
 clamp = 0.15
